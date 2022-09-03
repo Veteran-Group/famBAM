55f31e4f7650afe90b0cc77d9413011159004e83
Robert E. Campbell Jr <robert.e.campbell.jr@gmail.com>
2022-09-03 14:10:54 -0600

    login form layout complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, createContext } from 'react';
+import React, { useState, createContext, useEffect } from 'react';
 import logo from './assets/onering.png';
 import './App.css';
 import Login from './components/Login.jsx';
@@ -8,11 +8,12 @@ export const AppContext = createContext();
 
 function App() {
 
-  let [loginStatus, setLoginStatus] = useState();
+  let [loginStatus, setLoginStatus] = useState(false);
 
   return (
     <AppContext.Provider value={{loginStatus, setLoginStatus}}>
-      {loginStatus ?
+      {!loginStatus ?
+      <Login /> :
       <div className="App">
           <div className='App-logo-text'>FamBAM</div>
           <img src={logo} id="logo" className="App-logo" alt="logo" />
@@ -20,8 +21,7 @@ function App() {
             <button onClick={() => {setLoginStatus(loginStatus = switchStatus(loginStatus))}}>Switch Status</button>
             <button onClick={() => {alert(localStorage.getItem(`fambamLogin`))}}>Check login</button>
           </p>
-      </div> :
-      <Login />}
+      </div>}
     </AppContext.Provider>
   );
 }
--- src/lib/login.js
+++ src/lib/login.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 
 export const switchStatus = (loginStatus) => {
+  console.log(`Current Status: ${loginStatus}\nOpposite: ${!loginStatus}`)
   loginStatus = !loginStatus;
+  console.log(`New status:  ${loginStatus}`);
   localStorage.setItem('fambamLogin', `${loginStatus}`);
   return loginStatus;
 };
