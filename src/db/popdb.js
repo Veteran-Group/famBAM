c3b1c62c68143f0d79284716ae4ea54cdd85d08c
Robert Campbell Jr <robert.e.campbell.jr@gmail.com>
2022-09-09 12:04:49 -0600

    Delete popdb.js

--- src/db/popdb.js
+++
@@ -1,4 +0,0 @@
-const db = require(`./index.js`);
-
-db.queryAsync(`INSERT INTO fambamschema.profile (f_name, l_name, username) VALUES($1, $2, $3)`, ['Robert', 'Campbell', 'Dad'])
-  .then(() => db.queryAsync(`INSERT INTO fambamschema.credentiales (user_id, username, pass) VALUES($1, $2, $3)`, [1, 'Dad', 'B3y0urs3lf!']))
