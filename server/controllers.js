675d6a1359eba4a9df56b966dda4da2fe5f2ffc5
Robert E. Campbell Jr <robert.e.campbell.jr@gmail.com>
2022-09-13 17:43:11 -0600

    started working on textind Dad button see Twilio documentation

--- server/controllers.js
+++ server/controllers.js
@@ -50,4 +50,16 @@ module.exports = {
     )`))
     res.status(200).send(`New room '${desiredRoomName}' has been created!`)
   },
+  sendTextDad: function(req, res) {
+    const accountSid = process.env.TWILIO_ACCOUNT_SID;
+    const authToken = process.env.TWILIO_AUTH_TOKEN;
+    const client = require('twilio')(accountSid, authToken);
+
+    client.messages
+      .create({
+        body: 'IT IS ALIVE',
+        from: '+19162257301',
+        to: '+19162257301'
+      })
+    },
 }
