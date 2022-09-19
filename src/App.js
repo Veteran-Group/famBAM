83d95e0d2c88c15e8a86c0cb72227ccf842df64e
Robert E. Campbell Jr <robert.e.campbell.jr@gmail.com>
2022-09-19 17:44:01 -0600

    Separated chat server into separate server, Client-side connecting to chat server

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ import Todo from './components/Todo.jsx';
 import axios from 'axios';
 import UtilityBelt from './components/UtilityBelt.jsx';
 import { api } from './config.js';
+import { io } from 'socket.io-client';
+
+const socket = io('http://192.168.1.8:3002');
 
 export const AppContext = createContext();
 
@@ -50,6 +53,12 @@ function App() {
         setChatLog(chatLog = response.data);
       })
   }, []);
+
+  useEffect(() => {
+    socket.on('message', (message) => {
+      console.log(message);
+    })
+  })
 
   return (
     <AppContext.Provider value={{roomInfo, setRoomInfo, mainView, setMainView, profile, setProfile, loginStatus, setLoginStatus, chatLog, setChatLog}}>
